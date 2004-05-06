2147fac9c0e3535a468574df55ae58e443257a87
kjuen <>
2004-05-06 17:38:25 +0000

    license header added, traceback removed for Failures

--- R/testLogger.r
+++ R/testLogger.r
@@ -1,3 +1,21 @@
+##  RUnit : A unit test framework for the R programming language
+##  Copyright (C) 2003, 2004  Thomas Koenig, Matthias Burger, Klaus Juenemann
+##
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation; either version 2 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program; if not, write to the Free Software
+##  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
 newTestLogger <- function(useOwnErrorHandler) {
   ##@bdescr
   ## creates a new, empty TestLogger 'object'.
@@ -117,7 +135,7 @@ newTestLogger <- function(useOwnErrorHandler) {
     testData[[currentTestSuiteName]]$nFail <- 1 + testData[[currentTestSuiteName]]$nFail
 
     testData[[currentTestSuiteName]]$sourceFileResults[[currentSourceFileName]][[testFuncName]] <<-
-      list(kind="failure", msg=failureMsg, checkNo=checkNum, traceBack=currentTraceBack)
+      list(kind="failure", msg=failureMsg, checkNo=checkNum, traceBack=NULL)  ## traceBack is useless in this case
     currentTraceBack <<- NULL
   }
 
