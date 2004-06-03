6a7ac97533d63964991a4c0622bda6b637ad613d
burger <>
2004-06-03 11:59:13 +0000

    added test cases

--- inst/unitTests/runitRUnit.r
+++ inst/unitTests/runitRUnit.r
@@ -1,25 +1,21 @@
-######################################################################
+##  RUnit : A unit test framework for the R programming language
+##  Copyright (C) 2003, 2004  Thomas Koenig, Matthias Burger, Klaus Juenemann
 ##
-## RUnit
-## =====================================
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation; either version 2 of the License, or
+##  (at your option) any later version.
 ##
-##  runitRUnit.r
-## =====================================
-##  test the test functions
-##  
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
 ##
-## Dependencies
-## =====================================
+##  You should have received a copy of the GNU General Public License
+##  along with this program; if not, write to the Free Software
+##  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ##
-##
-## Version
-## =====================================
 ##  $Id$
-##
-## Copyright (c) 2004  Epigenomics AG Berlin - All rights reserved
-## THIS IS PROPRIETARY SOURCE CODE of Epigenomics AG Berlin
-##
-######################################################################
 
 
 cat("\n\nRUnit test cases for 'RUnit' functions\n\n")
@@ -27,17 +23,49 @@ cat("\n\nRUnit test cases for 'RUnit' functions\n\n")
 
 testRUnit.checkEquals <- function()
 {
-
+  checkTrue( TRUE)
+  checkException( checkTrue(FALSE))
   
+  checkTrue( checkEquals(9,9))
+  
+  checkTrue( checkEquals( numeric(1), numeric(1)))
+  checkTrue( checkEquals( character(1), character(1)))
+
+  checkTrue( checkEquals( matrix(1, 3,5), matrix(1, 3,5)))
+  checkTrue( checkEquals( matrix(1, 5000,5), matrix(1, 5000,5)))
+
+  checkTrue( checkEquals( expression(2), expression(2)))
+  checkTrue( checkEquals( list(100), list(100)))
 }
+
 
 testRUnit.checkEqualsNumeric <- function()
 {
 
+  checkTrue( checkEqualsNumeric( 9,9))
+  checkException( checkEqualsNumeric( 9, 10))
+  
 }
 
 
 testRUnit.checkTrue <- function()
 {
+  checkException( checkTrue( FALSE))
+  checkEquals( checkTrue( TRUE), TRUE)
+}
 
+
+testRUnit.checkException <- function()
+{
+
+  checkException( checkTrue( FALSE))
+  checkException( checkTrue( ))
+  checkException( checkEquals( ))
+  checkException( checkEquals( 24))
+  checkException( checkEquals( 24, 24, tol="dummy"))
+  checkException( checkEqualsNumeric( ))
+  checkException( checkEqualsNumeric( 24))
+  checkException( checkEqualsNumeric( 24, 24, tol="dummy"))
+
+  
 }
