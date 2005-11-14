1d798daf1ea0480fd4960db85e9f670872e61274
burger <>
2005-11-14 12:36:41 +0000

    used for unit test cases

---
+++ inst/examples/correctTestCase.r
@@ -0,0 +1,23 @@
+######################################################################
+##
+## library: RUnit
+## =====================================
+##
+## correctTestCase.r
+## =====================================
+##  test function definition which by construction executes wo error or failure
+##
+##
+##
+## Version
+## =====================================
+##  $Id$
+##
+##
+######################################################################
+
+test.correctTestCase <- function() {
+
+  checkTrue( TRUE)
+  checkTrue( !isTRUE(FALSE))
+}
