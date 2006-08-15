5c6e13d1441298bc761796a4bac605b64436d7af
burgerm <>
2006-08-15 16:34:11 +0000

    documentation tags updated

--- R/html.r
+++ R/html.r
@@ -21,68 +21,87 @@
 writeRaw <- function(htmlStr,htmlFile,append=TRUE)
 {
   ##@bdescr
+  ## private function
   ## write raw text in a html file
   ##@bdescr
   ##@in htmlStr  : [character] text
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
+  ##
+  ##@codestatus : internal
   
   cat(htmlStr,file=htmlFile,append=append)
   invisible(TRUE)
 }
 
+
 writeRawCR <- function(htmlStr,htmlFile,append=TRUE)
 {
   ##@bdescr
+  ## private function
   ## write raw text in a html file with a cr at end
   ##@bdescr
   ##@in htmlStr  : [character] text
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
-  
+  ##
+  ##@codestatus : internal 
+
   writeRaw(htmlStr,htmlFile,append);
   cat("\n",file=htmlFile,append=TRUE)
   invisible(TRUE)
 }
 
+
 writeTitle <- function(htmlStr,htmlFile,append=TRUE)
 {
   ##@bdescr
+  ## private function
   ## write title tags and title text
   ##@bdescr
   ##@in htmlStr  : [character] title
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
-  
+  ##
+  ##@codestatus : internal
+   
   writeRaw("<title>",htmlFile,append);
   writeRaw(htmlStr,htmlFile);
   writeRaw("</title>\n",htmlFile);
 }
 
+
 writeBeginHead <- function(htmlFile,append=TRUE)
 {
   ##@bdescr
+  ## private function
   ## write <head>
   ##@bdescr
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
-  
+  ##
+  ##@codestatus : internal
+    
   writeRaw("<head>",htmlFile,append);
 }
+
 
 writeEndHead <- function(htmlFile,append=TRUE)
 {
   ##@bdescr
+  ## private function
   ## write </head>
   ##@bdescr
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
-  
+  ##
+  ##@codestatus : internal
+    
   writeRaw("</head>\n",htmlFile,append);
 }
 
@@ -90,54 +109,71 @@ writeEndHead <- function(htmlFile,append=TRUE)
 writeBeginHtml <- function(htmlFile,append=TRUE)
 {
   ##@bdescr
+  ## private function
   ## write <html>
   ##@bdescr
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
-  
+  ##
+  ##@codestatus : internal
+    
   writeRaw("<html>",htmlFile,append);
 }
+
 
 writeEndHtml <- function(htmlFile,append=TRUE)
 {
   ##@bdescr
+  ## private function
   ## write </html>
   ##@bdescr
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
-  
+  ##
+  ##@codestatus : internal
+    
   writeRaw("</html>\n",htmlFile,append);
 }
+
 
 writeBeginBody <- function(htmlFile,append=TRUE)
 {
   ##@bdescr
+  ## private function
   ## write <body>
   ##@bdescr
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
-  
+  ##
+  ##@codestatus : internal
+    
   writeRaw("<body>",htmlFile,append);
 }
+
 
 writeEndBody <- function(htmlFile,append=TRUE)
 {
   ##@bdescr
+  ## private function
   ## write </body>
   ##@bdescr
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
-  
+  ##
+  ##@codestatus : internal
+    
   writeRaw("</body>\n",htmlFile,append);
 }
+
 
 writeBeginTag <- function(htmlTag,htmlFile,para="",append=TRUE)
 {
   ##@bdescr
+  ## private function
   ## write begin of a tag, with parameters
   ##@bdescr
   ##@in htmlTag  : [character] name of the tag
@@ -145,7 +181,9 @@ writeBeginTag <- function(htmlTag,htmlFile,para="",append=TRUE)
   ##@in para     : [character] parameters as string
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
-  
+  ##
+  ##@codestatus : internal
+   
   if(para =="")
   {
     writeRaw(paste("<",htmlTag,">",sep=""),htmlFile,append);
@@ -157,39 +195,48 @@ writeBeginTag <- function(htmlTag,htmlFile,para="",append=TRUE)
 
 }
 
+
 writeEndTag <- function(htmlTag,htmlFile,append=TRUE)
 {
   ##@bdescr
+  ## private function
   ## write end of tag
   ##@bdescr
   ##@in htmlTag  : [character] name of the tag
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
-  
+  ##
+  ##@codestatus : internal
+    
   writeRaw(paste("</",htmlTag,">",sep=""),htmlFile,append);
 }
+
 
 writeCR <- function(htmlFile,append=TRUE)
 {
   ##@bdescr
+  ## private function
   ## write CR in html file for better formatting of the html source
   ##@bdescr
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
   ##@ret         : [logical] TRUE if execution completes
-  
+  ##
+  ##@codestatus : internal
+    
   cat("\n",file=htmlFile,append=append)
   invisible(TRUE)
 }
+
 
 writeBeginTable <- function(header,htmlFile,border=1,
                             width="100%",append=TRUE,
                             columnWidth=NULL)
 {
-
   ##@bdescr
+  ## private function
   ## write begin of a table
   ##@bdescr
   ##@in header   : [character] title for columns
@@ -198,7 +245,9 @@ writeBeginTable <- function(header,htmlFile,border=1,
   ##@in width    : [character] width of table
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
-
+  ##
+  ##@codestatus : internal
+  
   tablePara <- paste("border=\"",border,"\" width=\"",width,"\"",sep="")
   writeRawCR(paste("<table ",tablePara," >",sep=""),htmlFile,append)
 
@@ -229,10 +278,11 @@ writeBeginTable <- function(header,htmlFile,border=1,
   writeCR(htmlFile)
 }
 
+
 writeTableRow <- function(row,htmlFile,append=TRUE,bgcolor="")
 {
-
   ##@bdescr
+  ## private function
   ## write a table row
   ##@bdescr
   ##@in row      : [character] data for table cells in row
@@ -240,7 +290,9 @@ writeTableRow <- function(row,htmlFile,append=TRUE,bgcolor="")
   ##@in append   : [logical] append the html code
   ##@in bgcolor  : [character] color for table cells
   ##@ret         : [logical] TRUE if execution completes
-  
+  ##
+  ##@codestatus : internal
+    
   writeBeginTag("tr",htmlFile);
   if(length(bgcolor) == 1)
   {
@@ -269,40 +321,57 @@ writeTableRow <- function(row,htmlFile,append=TRUE,bgcolor="")
 writeLink <- function(target,name,htmlFile,append=TRUE)
 {
   ##@bdescr
-  ##  write a link
+  ## private function
+  ## write a link
   ##@bdescr
   ##@in target   : [character] target of the link
   ##@in name     : [character] name of the target
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
-  
+  ##
+  ##@codestatus : internal
+    
   writeBeginTag("a",htmlFile,paste("href=\"",target,"\"",sep=""),append=append);
   writeRaw(name,htmlFile,append=TRUE);
   writeEndTag("a",htmlFile,append=TRUE);
 }
 
+
 writeEndTable <- function(htmlFile,append=TRUE)
 {
   ##@bdescr
-  ##  
+  ## private function
+  ## close an <table> enviroment by adding </table>
   #@bdescr
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
+  ##
+  ##@codestatus : internal
   
   writeEndTag("table",htmlFile,append);
   writeCR(htmlFile);
 }
 
+
 writeHtmlHeader <- function(header,htmlFile)
 {
   ##@bdescr
+  ## private function
   ## write a HTML file header
+  ##  - DOCTYPE
+  ##  - <html>
+  ##  - <head> <title> </head>
+  ##  - <body>
+  ##
+  ## should be finished by writeHtmlEnd 
   ##@bdescr
   ##@in header   : [character] title of the document
   ##@in htmlFile : [character] name of the link
   ##@ret         : [logical] TRUE if execution completes
+  ##
+  ##@codestatus : internal
   
   writeRawCR("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"",htmlFile,FALSE);
   writeRawCR("\"http://www.w3.org/TR/html4/transitional.dtd\">",htmlFile);
@@ -313,25 +382,33 @@ writeHtmlHeader <- function(header,htmlFile)
   writeBeginBody(htmlFile);
 }
 
+
 writeHtmlEnd <- function(htmlFile)
 {
   ##@bdescr
+  ## private function
   ## write end of html code
   ##@bdescr
   ##@in htmlFile : [character] name of the html file
   ##@ret         : [logical] TRUE if execution completes
+  ##
+  ##@codestatus : internal
   
   writeEndBody(htmlFile);
   writeEndHtml(htmlFile);
 }
 
+
 writeHtmlSep <- function(htmlFile)
 {
   ##@bdescr
+  ## private function
   ## write horizontal seperator
   ##@bdescr
   ##@in htmlFile : [character] name of the html file
   ##@ret         : [logical] TRUE if execution completes
+  ##
+  ##@codestatus : internal
   
   writeRawCR("<hr>",htmlFile);
 }
@@ -340,12 +417,15 @@ writeHtmlSep <- function(htmlFile)
 writeImage <- function(img,htmlFile,append=TRUE)
 {
   ##@bdescr
+  ## private function
   ## write image tags
   ##@bdescr
   ##@in img :      [character] name of the image file
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
+  ##
+  ##@codestatus : internal
   
   writeBeginTag("img",htmlFile,para=paste("src=\"",img,"\"",sep=""),append);
   writeEndTag("img",htmlFile);
@@ -355,6 +435,7 @@ writeImage <- function(img,htmlFile,append=TRUE)
 writeHtmlSection <- function(title,sec,htmlFile,append=TRUE)
 {
   ##@bdescr
+  ## private function
   ## write titles for section
   ##@bdescr
   ##@in title    : [character] title of the section
@@ -362,6 +443,8 @@ writeHtmlSection <- function(title,sec,htmlFile,append=TRUE)
   ##@in htmlFile : [character] name of the html file
   ##@in append   : [logical] append the html code
   ##@ret         : [logical] TRUE if execution completes
+  ##
+  ##@codestatus : internal
   
   secTag <- paste("h",sec,sep="")
   writeBeginTag(secTag,htmlFile,append);
@@ -375,7 +458,8 @@ writeHtmlTable <- function(dataFrame, htmlFile, border=1,
                            width="100%", append=TRUE)
 {
   ##@bdescr
-  ##  writes a data frame to a HTML table 
+  ## private function
+  ## write a data frame to a HTML table 
   ##@bdescr
   ##
   ##@in dataFrame : [data frame] size of title (between 1-6)
@@ -385,7 +469,7 @@ writeHtmlTable <- function(dataFrame, htmlFile, border=1,
   ##@in append    : [logical] if TRUE append the tabel to an existing HTML file
   ##@ret          : [logical] TRUE if execution completed
   ##
-  ##@codestatus   : untested
+  ##@codestatus   : internal
 
   header <- NULL
   colNames <- colnames(dataFrame)
