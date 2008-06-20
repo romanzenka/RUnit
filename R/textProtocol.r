561cc4db777b4be33723e0e84302d27e5ce64d30
burgerm <>
2008-06-20 15:51:30 +0000

    printTextProtocol rewritten by Klaus using toText; getErrors moved here, TODO replace with S4 method

--- R/textProtocol.r
+++ R/textProtocol.r
@@ -20,256 +20,105 @@
 printTextProtocol <- function(testData,
                               fileName = "",
                               separateFailureList = TRUE,
-                              showDetails = TRUE,
-                              traceBackCutOff=9) {
-
+                              showDetails = TRUE) {
   ##@bdescr
-  ##  Report generator
-  ##  Extracts the log information stored in the 'RUnitTestData' test run object
-  ##  and generates a well formated output.
+  ## Report generator
+  ## Creates text-based test protocol from RUnit TestResult object.
+  ## The output  format is compatible with (X)Emacs ouline mode.
   ##@edescr
   ##
-  ##@in  testData            : [RUnitTestData] S3 class object
+  ##@in  testData            : [TestResult] S4 class object
   ##@in  fileName            : [character] string, fulll path + file name to be written to
   ##@in  separateFailureList : [logical] if TRUE (default) add a failure list
   ##@in  showDetails         : [logical] if TRUE (default) add detailed trackbacks for each error incurred
-  ##@in  traceBackCutOff     : [integer] number of steps back in the trace back stack to display
   ##@ret                     : [logical] TRUE if execution completed wo error
-  ##
-  ##@codestatus : testing
-
-  ##  preconditions
-  if (!is(testData, "RUnitTestData"))
-  {
-    stop("Argument 'testData' must be of class 'RUnitTestData'.")
-  }
-
-  if (!is.character(fileName))
-  {
-    stop("Argument 'fileName' has to be of type character.")
-  }
-  if (length(fileName) != 1)
-  {
-    stop("Argument 'fileName' must contain exactly one element.")
-  }
-
-  if (!is.logical(separateFailureList))
-  {
-    stop("Argument 'separateFailureList' has to be of type logical.")
-  }
-  if (length(separateFailureList) != 1)
-  {
-    stop("Argument 'separateFailureList' must contain exactly one element.")
-  }
-
-  if (!is.logical(showDetails))
-  {
-    stop("Argument 'showDetails' has to be of type logical.")
-  }
-  if (length(showDetails) != 1)
-  {
-    stop("Argument 'showDetails' must contain exactly one element.")
-  }
-
-  if (!is.numeric(traceBackCutOff))
-  {
-    stop("Argument 'traceBackCutOff' has to be of type logical.")
-  }
-  if (length(traceBackCutOff) != 1)
-  {
-    stop("Argument 'traceBackCutOff' must contain exactly one element.")
-  }
-  if (traceBackCutOff < 0 || traceBackCutOff > 100)
-  {
-    stop("Argument 'traceBackCutOff' out of valid range [0, 100].")
-  }
-
-
-  ## just a convenience function
-  pr <- function(..., sep=" ", nl=TRUE) {
-    if(nl) {
-      cat(... , "\n", file = fileName, append=TRUE, sep=sep)
-    }
-    else {
-      cat(... , file = fileName, append=TRUE, sep=sep)
-    }
-  }
-
-  ## get singular or plural right
-  sop <- function(number, word, plext="s")
-  {
-    ifelse(number == 1, paste(number, word),
-           paste(number, paste(word, plext, sep="")))
-  }
-
-  ## header part
-  cat("RUNIT TEST PROTOCOL --", date(), "\n", file = fileName)
-  pr("***********************************************")
-  if(length(testData) == 0) {
-    pr("no test cases :-(")
-    return(invisible(TRUE))
-  }
-
-  errInfo <- getErrors(testData)
-  pr("Number of test functions:", errInfo$nTestFunc)
-  if(errInfo$nDeactivated > 0) {
-    pr("Number of deactivated test functions:", errInfo$nDeactivated)
-  }
-  pr("Number of errors:", errInfo$nErr)
-  pr("Number of failures:", errInfo$nFail, "\n\n")
-
-
-  ## summary of test suites
-  pr(sop(length(testData), "Test Suite"), ":")
-  for(tsName in names(testData)) {
-    pr(tsName, " - ", sop(testData[[tsName]]$nTestFunc, "test function"), ", ",
-       sop(testData[[tsName]]$nErr, "error"), ", ",
-       sop(testData[[tsName]]$nFail, "failure"), sep="")
-    if(separateFailureList && (testData[[tsName]]$nErr + testData[[tsName]]$nFail > 0)) {
-      srcFileRes <- testData[[tsName]][["sourceFileResults"]]
-      for(i in seq_along(srcFileRes)) {
-        testFuncNames <- names(srcFileRes[[i]])
-        for(j in seq_along(testFuncNames)) {
-          funcList <- srcFileRes[[i]][[testFuncNames[j]]]
-          if(funcList$kind == "error") {
-            pr("ERROR in ", testFuncNames[j], ": ", funcList$msg, nl=FALSE, sep="")
-          }
-          else if(funcList$kind == "failure") {
-            pr("FAILURE in ", testFuncNames[j], ": ", funcList$msg,
-               sep="", nl=FALSE)
-          }
-          else if(funcList$kind == "deactivated") {
-            pr("DEACTIVATED ", testFuncNames[j], ": ", funcList$msg,
-               sep="", nl=FALSE)
-          }
-        }
-      }
-    }
-  }
-
-
-  ## if no details are required, we are done.
-  if(!showDetails) {
-    return(invisible(TRUE))
-  }
   
-  pr("\n\n\nDetails")
-
-  ## loop over all test suites
-  for(tsName in names(testData)) {
-    tsList <- testData[[tsName]]
-    pr("***************************")
-    pr("Test Suite:", tsName)
-    pr("Test function regexp:", tsList$testFuncRegexp)
-    pr("Test file regexp:", tsList$testFileRegexp)
-    if(length(tsList$dirs) == 0) {
-      pr("No directories !")
-    }
-    else {
-      if(length(tsList$dirs) == 1) {
-        pr("Involved directory:")
-      }
-      else {
-        pr("Involved directories:")
-      }
-      for(dir in tsList$dirs) {
-        pr(dir)
-      }
-      res <- tsList$sourceFileResults
-      testFileNames <- names(res)
-      if(length(res) == 0) {
-        pr("no test files")
-      }
-      else {
-        ## loop over all source files
-        for(testFileName in testFileNames) {
-          testFuncNames <- names(res[[testFileName]])
-          if(length(testFuncNames) > 0) {
-            pr("---------------------------")
-            pr("Test file:", testFileName)
-            ## loop over all test functions in the test file
-            for(testFuncName in testFuncNames) {
-              testFuncInfo <- res[[testFileName]][[testFuncName]]
-              if(testFuncInfo$kind == "success") {
-                pr(testFuncName, ": (",testFuncInfo$checkNum, " checks) ... OK (", testFuncInfo$time, " seconds)", sep="")
-              }
-              else {
-                if(testFuncInfo$kind == "error") {
-                  pr(testFuncName, ": ERROR !! ", sep="")
-                }
-                else if (testFuncInfo$kind == "failure") {
-                  pr(testFuncName, ": FAILURE !! (check number ", testFuncInfo$checkNum, ")", sep="")
-                }
-                else if (testFuncInfo$kind == "deactivated") {
-                  pr(testFuncName, ": DEACTIVATED, ", nl=FALSE)
-                }
-                else {
-                  pr(testFuncName, ": unknown error kind", sep="")
-                }
-                pr(testFuncInfo$msg, nl=FALSE)
-                if(length(testFuncInfo$traceBack) > 0) {
-                  pr("   Call Stack:")
-                  if(traceBackCutOff > length(testFuncInfo$traceBack)) {
-                    pr("   (traceBackCutOff argument larger than length of trace back: full trace back printed)")
-                    for(i in 1:length(testFuncInfo$traceBack)) {
-                      pr("   ", i, ": ", testFuncInfo$traceBack[i], sep="")
-                    }
-                  }
-                  else {
-                    for(i in traceBackCutOff:length(testFuncInfo$traceBack)) {
-                      pr("   ", 1+i-traceBackCutOff, ": ", testFuncInfo$traceBack[i], sep="")
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+  ## Check input arguments
+  flag <- isValidTestResult(testData)
+  if(!isTRUE(flag)) {
+    stop(flag)
+  }
+  if (! (is.character(fileName) && length(fileName)==1) )  {
+    stop("Argument 'fileName' has to be one character string.")
+  }
+  if (! (is.logical(separateFailureList) && length(separateFailureList) == 1) ) {
+    stop("Argument 'separateFailureList' has to be one logical.")
+  }
+  if (! (is.logical(showDetails) && length(showDetails) == 1) ) {
+    stop("Argument 'showDetails' has to be one logical.")
   }
 
-  ##  return type  
+  ## local helper functions
+  lcat <- function(...) cat(..., file=fileName, append=TRUE)
+  lcatn <- function(x) lcat(x, "\n")
+  nSuites <- length(testData@testSuiteData)
+  
+  ## header
+  cat("RUNIT TEST PROTOCOL --", date(), "\n", file = fileName)
+  lcat("-----------------------------------------------\n")
+  
+  lcat("\n* Summary\n")
+  lapply(toText(testData, 0), lcatn)
+  lcat("\n\n")
+  
+  ## optional list of all errors, failures and deactivations
+  if(separateFailureList &&
+     length(testFuncsByState(testData, c("ERROR", "FAILURE", "DEACTIVATED"))) > 0) {
+    lcat("* Errors and Failures\n")
+    lapply(testData@testSuiteData, function(ts) {
+      problems <- testFuncsByState(ts, c("ERROR", "FAILURE", "DEACTIVATED"))
+      if( (nSuites > 1) && length(problems) > 0) {
+        lcat("** ", toText(ts@testSuite, 0), "\n")
+        lapply(problems, function(x) lapply(toText(x, 0), lcatn))
+      }
+    })
+  }
+  lcat("\n\n")
+
+  ## optional: print out all details that we have
+  if(showDetails) {
+    lcat("* Details\n\n")
+    lapply(testData@testSuiteData, function(ts) {
+      tmp <- toText(ts,2)
+
+      ## add *s for outline mode
+      tmp[1] <- paste("**", tmp[1])
+      tmp <- sub("^Test file", "*** Test file", tmp)
+      
+      lapply(tmp, lcatn)
+      lcat("\n")
+    })
+
+    ## print some important system information
+    lcat("\n* System Information\n")
+    lcat(testData@systemInfo$sessionInfo$R.version$version.string, "\n")
+    lcat("Installed packages: ",
+         c(testData@systemInfo$sessionInfo$basePkgs,
+           names(testData@systemInfo$sessionInfo$otherPkgs)), "\n")
+
+  }
+
   return(invisible(TRUE))
 }
 
 
-print.RUnitTestData <- function(x, ...)
-{
-  ##@bdescr
-  ##  Generic print method
-  ##@edescr
-  ##
-  ##@in  x   : [RUnitTestData] S3 class object
-  ##@in  ... : [ANY] currently ignored
-  ##@ret     : [NULL]
-  ##
-  ##@codestatus : untested
-  
-  errInfo <- getErrors(x)
-  cat("Number of test functions:", errInfo$nTestFunc, "\n")
-  if(errInfo$nDeactivated > 0) {
-    cat("Number of deactivated test functions:", errInfo$nDeactivated, "\n")
+
+
+getErrors <- function(obj) {
+  ## provided for backwards compatibility
+
+  flag <- isValidTestResult(obj)
+  if(!isTRUE(flag)) {
+    stop(flag)
   }
-  cat("Number of errors:", errInfo$nErr, "\n")
-  cat("Number of failures:", errInfo$nFail, "\n")
   
-}
-
-
-summary.RUnitTestData <- function(object, ...)
-{
-
-  ##@bdescr
-  ##  Generic summary method
-  ##@edescr
-  ##
-  ##@in  object : [RUnitTestData] S3 class object
-  ##@in  ...    : [ANY]
-  ##@ret        : [logical] return valof from printTextProtocol
-  ##
-  ##@codestatus : untested
+  count <- countResult(obj)
+  ret <- list(nErr=0, nDeactivated=0, nFail=0, nTestFunc=0)
   
-  printTextProtocol(object, ...)
+  ret$nErr <- as.numeric(count["ERROR"])
+  ret$nDeactivated <- as.numeric(count["DEACTIVATED"])
+  ret$nFail <- as.numeric(count["FAILURE"])
+  ret$nTestFunc <- as.numeric(sum(count))
+  return(ret)
 }
 
