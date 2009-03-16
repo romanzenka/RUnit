f977f6c4f59c70d47c55e290492ff0fa113c886d
burgerm <>
2009-03-16 16:26:14 +0000

    always coninue if-else on the same line removed semicolon at line ends

--- R/exportHTML.r
+++ R/exportHTML.r
@@ -1,5 +1,5 @@
 ##  RUnit : A unit test framework for the R programming language
-##  Copyright (C) 2003-2006  Thomas Koenig, Matthias Burger, Klaus Juenemann
+##  Copyright (C) 2003-2009  Thomas Koenig, Matthias Burger, Klaus Juenemann
 ##
 ##  This program is free software; you can redistribute it and/or modify
 ##  it under the terms of the GNU General Public License as published by
@@ -37,23 +37,22 @@ plotConnection.trackInfo <- function(con, pngfile, ...)
   ## create nothing if 
   if(all(con==0))
   {
-    
     ## open png device
-    png(filename=pngfile,width=1024,height=960);
-    plot(1:10,axes=FALSE,xlab="",ylab="",main="",type="n");
-    text(5,5,labels="No connection graph available");
-    dev.off();
-    return(invisible());
+    png(filename=pngfile,width=1024,height=960)
+    plot(1:10,axes=FALSE,xlab="",ylab="",main="",type="n")
+    text(5,5,labels="No connection graph available")
+    dev.off()
+    return(invisible())
   }
   
   ## overall connections
-  allCon <- sum(con);
+  allCon <- sum(con)
 
   ## connections with percent
-  con <- ceiling(con/sum(con)*100);
+  con <- ceiling(con/sum(con)*100)
 
   ## normalize for colors
-  con <- (con + 14) %/% 15;
+  con <- (con + 14) %/% 15
 
   ## open png device
   png(filename=pngfile,width=1024,height=960)
@@ -76,7 +75,6 @@ plotConnection.trackInfo <- function(con, pngfile, ...)
   {
     for(j in 1:ncol(con))
     {
-
       ## check for an existing connection
       if(con[i,j] != 0)
       {
@@ -104,9 +102,8 @@ plotConnection.trackInfo <- function(con, pngfile, ...)
           arrows(splTop$x[l-1], splTop$y[l-1], splTop$x[l], splTop$y[l],
                  length=0.04, col=colDraw)
           offset[from] <- offset[from] + 1
-        }
-        else
-        {
+          
+        } else {
           ## "regular" case
           middle <- (i+j)/2;
           splxy <- spline(c(from - 0.2, middle, to + 0.2),
@@ -115,13 +112,11 @@ plotConnection.trackInfo <- function(con, pngfile, ...)
           
           if(i < j)
           {
-            l <- length(splxy$y);
+            l <- length(splxy$y)
             ## draw an arrow tip
             arrows(splxy$y[l-1], splxy$x[l-1], splxy$y[l], splxy$x[l],
                    length=0.06, col=colDraw)
-          }
-          else
-          {
+          } else {
             ## draw "inverse" arrow tip
             arrows(splxy$y[2], splxy$x[2], splxy$y[1], splxy$x[1],
                    length=0.06, col=colDraw)
@@ -160,74 +155,71 @@ printHTML.trackInfo <- function(object, baseDir=".")
   ##@codestatus :  untested
    
   ##  preconditions
-  if (!is(object, "trackInfo"))
-  {
+  if (!is(object, "trackInfo")) {
     stop("argument 'object' has to be a list of class 'trackInfo'.")
   }
   
-  if (!is.character(baseDir))
-  {
+  if (!is.character(baseDir)) {
     stop("argument 'baseDir' has to be of type 'character'.")
   }
-  if (length(baseDir) != 1)
-  {
+  if (length(baseDir) != 1) {
     stop("argument 'baseDir' has to contain exactly one element.")
   }
-
+  if (is.na(baseDir)) {
+    stop("argument 'baseDir' may not be missing value.")
+  }
   
   path <- file.path(baseDir,"results")
   if (!file.exists(path))
   {
     ok <- dir.create(path)
-    if(!ok)
-    {
+    if(!ok) {
       stop(paste("could not create", path) )
     }
   }
   htmlFile <- file.path(path,"index.html")
 
-  footerString <- paste("RUnit ", packageDescription("RUnit", fields="Version"), as.character(Sys.time()))
+  footerString <- paste("RUnit ", packageDescription("RUnit", fields="Version"),
+                        as.character(Sys.time()))
   
   ## create index.html
-  writeHtmlHeader("RUnit Code Inspection - Overview",htmlFile);
-  writeHtmlSection("Overview",2,htmlFile);
+  writeHtmlHeader("RUnit Code Inspection - Overview",htmlFile)
+  writeHtmlSection("Overview",2,htmlFile)
   writeBeginTable(c("Categ.","Name","Signature"),htmlFile)
   for(i in seq_along(object))
   {
-    funcID <- strsplit(names(object)[i],"/")[[1]];
+    funcID <- strsplit(names(object)[i],"/")[[1]]
     funcCat <- funcID[1]
     funcName <- funcID[2]
     if(length(funcID) > 2)
     {
-      sig <- funcID[3:length(funcID)];
+      sig <- funcID[3:length(funcID)]
       funcSig <- paste(funcName,"(",paste(sig,collapse=", "),")",sep="")
-    }
-    else
-    {
-      funcSig <- "";
+    } else {
+      funcSig <- ""
     }
 
-    writeBeginTag("tr",htmlFile);
-    writeCR(htmlFile);
+    writeBeginTag("tr",htmlFile)
+    writeCR(htmlFile)
     
     ## write function category
-    writeBeginTag("td",htmlFile);
-    writeRaw(funcCat,htmlFile);
-    writeEndTag("td",htmlFile);    
-    writeCR(htmlFile);
+    writeBeginTag("td",htmlFile)
+    writeRaw(funcCat,htmlFile)
+    writeEndTag("td",htmlFile)
+    writeCR(htmlFile)
 
     ## write function name
-    writeBeginTag("td",htmlFile);
+    writeBeginTag("td",htmlFile)
     writeLink(file.path(".", paste("result",i,".html",sep="")), funcName, htmlFile)
-    writeEndTag("td",htmlFile);    
-    writeCR(htmlFile);
+    writeEndTag("td",htmlFile)
+    writeCR(htmlFile)
 
     ## write function signature
-    writeBeginTag("td",htmlFile);
-    writeRaw(funcSig,htmlFile);
-    writeEndTag("td",htmlFile);    
-    writeCR(htmlFile);
-    writeEndTag("tr",htmlFile);
+    writeBeginTag("td",htmlFile)
+    writeRaw(funcSig,htmlFile)
+    writeEndTag("td",htmlFile)
+    writeCR(htmlFile)
+    writeEndTag("tr",htmlFile)
   }
   writeEndTable(htmlFile)
   writeRaw(footerString, htmlFile)
@@ -236,21 +228,21 @@ printHTML.trackInfo <- function(object, baseDir=".")
 
   writeLinkRef <- function(htmlFile,leftLink,leftName,rightLink,rightName)
   {
-    writeBeginTable(c("",""),htmlFile,border=0,width="100%");
+    writeBeginTable(c("",""),htmlFile,border=0,width="100%")
 
-    writeBeginTag("tr",htmlFile);
-    writeCR(htmlFile);
+    writeBeginTag("tr",htmlFile)
+    writeCR(htmlFile)
 
-    writeBeginTag("td",htmlFile);
+    writeBeginTag("td",htmlFile)
     writeLink(leftLink,leftName,htmlFile)
-    writeEndTag("td",htmlFile);    
-    writeCR(htmlFile);
+    writeEndTag("td",htmlFile)  
+    writeCR(htmlFile)
 
     writeBeginTag("td",htmlFile,"align=\"right\"");
     writeLink(rightLink,rightName,htmlFile)
-    writeEndTag("td",htmlFile);    
-    writeEndTag("tr",htmlFile);    
-    writeCR(htmlFile);
+    writeEndTag("td",htmlFile) 
+    writeEndTag("tr",htmlFile) 
+    writeCR(htmlFile)
 
     writeEndTable(htmlFile)
   }
@@ -267,29 +259,29 @@ printHTML.trackInfo <- function(object, baseDir=".")
     htmlFile <- file.path(path, paste("result",i,".html",sep=""))
 
     ## begin result page
-    writeHtmlHeader("RUnit Code Inspection - Result",htmlFile);
+    writeHtmlHeader("RUnit Code Inspection - Result",htmlFile)
 
 
-    writeLinkRef(htmlFile,"index.html","index",relGraphFile,"graph");
+    writeLinkRef(htmlFile,"index.html","index",relGraphFile,"graph")
     
-    writeHtmlSep(htmlFile);
-    writeHtmlSection("Result",2,htmlFile);
+    writeHtmlSep(htmlFile)
+    writeHtmlSection("Result",2,htmlFile)
 
-    funcName <- strsplit(names(object)[i],"/")[[1]][2]; 
-    writeRaw("Function: ",htmlFile);
-    writeBeginTag("b",htmlFile);
-    writeRaw(funcName,htmlFile);
-    writeEndTag("b",htmlFile);
-    writeCR(htmlFile);
+    funcName <- strsplit(names(object)[i],"/")[[1]][2]
+    writeRaw("Function: ",htmlFile)
+    writeBeginTag("b",htmlFile)
+    writeRaw(funcName,htmlFile)
+    writeEndTag("b",htmlFile)
+    writeCR(htmlFile)
 
-    writeRaw("Runs: ",htmlFile);
-    writeBeginTag("b",htmlFile);
-    writeRaw(object[[i]]$nrRuns,htmlFile);
-    writeEndTag("b",htmlFile);
-    writeCR(htmlFile);
+    writeRaw("Runs: ",htmlFile)
+    writeBeginTag("b",htmlFile)
+    writeRaw(object[[i]]$nrRuns,htmlFile)
+    writeEndTag("b",htmlFile)
+    writeCR(htmlFile)
 
     
-    writeCR(htmlFile);
+    writeCR(htmlFile)
 
     writeBeginTable(c("line","code","calls","time"),htmlFile)
     for(j in seq_along(object[[i]]$src))
@@ -305,9 +297,7 @@ printHTML.trackInfo <- function(object, baseDir=".")
       if(object[[i]]$run[j] > 0)
       {
         bgcolor <- "#00D000"
-      }
-      else
-      {
+      } else {
         bgcolor <- "#D00000"
       }
       writeTableRow(c(j,srcLine,object[[i]]$run[j],round(object[[i]]$time[j],2)),
@@ -322,7 +312,7 @@ printHTML.trackInfo <- function(object, baseDir=".")
     writeHtmlEnd(htmlFile)
 
     ##  Conncetion plot
-    plotConnection.trackInfo(object[[i]]$graph,absGraphImg)
+    plotConnection.trackInfo(object[[i]]$graph, absGraphImg)
     writeHtmlHeader("RUnit Code Inspection - Connection Graph",absGraphFile)
     writeLinkRef(absGraphFile,"index.html","index",relHTMLFile,"Function")
     writeHtmlSep(absGraphFile)
