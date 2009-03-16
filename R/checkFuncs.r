77fc51c48bffe82d626463a12af3cd8fe4262455
burgerm <>
2009-03-16 16:29:30 +0000

    always coninue if else on the same line checkIdentical: added precondition check

--- R/checkFuncs.r
+++ R/checkFuncs.r
@@ -1,5 +1,5 @@
 ##  RUnit : A unit test framework for the R programming language
-##  Copyright (C) 2003-2008  Thomas Koenig, Matthias Burger, Klaus Juenemann
+##  Copyright (C) 2003-2009  Thomas Koenig, Matthias Burger, Klaus Juenemann
 ##
 ##  This program is free software; you can redistribute it and/or modify
 ##  it under the terms of the GNU General Public License as published by
@@ -27,7 +27,7 @@ checkEquals <- function(target, current, msg="",
 
   ##@edescr
   ##@in  target    : [ANY] one thing to be compared
-  ##@in  current   : [ANY] the second object to be compared
+  ##@in  current   : [ANY] second object to be compared
   ##@in  msg       : [character] an optional message to further identify and document the call
   ##@in  tolerance : [numeric] directly passed to 'all.equal', see there for further documentation
   ##@in  checkNames: [logical] iff TRUE do not strip names attributes from current and target prior to the comparison
@@ -36,7 +36,7 @@ checkEquals <- function(target, current, msg="",
   ##@codestatus : testing
 
   if (missing(current)) {
-     stop("argument 'current' is missing.")
+     stop("argument 'current' is missing")
   }
   if(!is.numeric(tolerance)) {
     stop("'tolerance' has to be a numeric value")
@@ -63,8 +63,7 @@ checkEquals <- function(target, current, msg="",
       .testLogger$setFailure()
     }
     stop(paste(result, collapse="\n"), msg)
-  }
-  else {
+  } else {
     return(TRUE)
   }
 }
@@ -78,7 +77,7 @@ checkEqualsNumeric <- function(target, current, msg="", tolerance = .Machine$dou
   ## current and target are converted via as.vector() thereby stripping all attributes.
   ##@edescr
   ##@in target    : [ANY] one thing to be compared
-  ##@in current   : [ANY] the second object to be compared
+  ##@in current   : [ANY] second object to be compared
   ##@in tolerance : [numeric] directly passed to 'all.equal.numeric', see there for further documentation
   ##@in msg       : [character] an optional message to further identify and document the call
   ##
@@ -87,7 +86,7 @@ checkEqualsNumeric <- function(target, current, msg="", tolerance = .Machine$dou
   ##@codestatus : testing
 
   if (missing(current)) {
-    stop("argument 'current' is missing.")
+    stop("argument 'current' is missing")
   }
   if(!is.numeric(tolerance)) {
     stop("'tolerance' has to be a numeric value")
@@ -106,8 +105,7 @@ checkEqualsNumeric <- function(target, current, msg="", tolerance = .Machine$dou
       .testLogger$setFailure()
     }
     stop(paste(result, collapse="\n"), msg)
-  }
-  else {
+  } else {
     return(TRUE)
   }
 }
@@ -119,27 +117,28 @@ checkIdentical <- function(target, current, msg="")
   ## checks if two objects are exactly identical, thin convenience wrapper around 'identical'
   ##
   ##@edescr
-  ##@in target   : [ANY] one onject to be compared
+  ##@in target   : [ANY] one object to be compared
   ##@in current  : [ANY] second object to be compared
   ##@in msg      : [character] an optional message to further identify and document the call
   ##
   ##@ret         : [logical] TRUE, if objects 'target' and 'current' are identical
   ##
   ##@codestatus : testing
-  
+
+  if (missing(current)) {
+    stop("argument 'current' is missing")
+  }
   if(exists(".testLogger", envir=.GlobalEnv)) {
     .testLogger$incrementCheckNum()
   }
   
-  ##  strip attributes before comparing current and target
   result <- identical(target, current)
   if (!identical(TRUE, result)) {
     if(exists(".testLogger", envir=.GlobalEnv)) {
       .testLogger$setFailure()
     }
     stop(paste(paste(result, collapse="\n"), msg))
-  }
-  else {
+  } else {
     return(TRUE)
   }
 }
@@ -150,7 +149,6 @@ checkTrue <- function(expr, msg="")
   ##@bdescr
   ## checks whether or not something is true
   ##@edescr
-  ##
   ##@in expr : [expression] the logical expression to be checked to be TRUE
   ##@in msg  : [character] optional message to further identify and document the call
   ##
@@ -159,7 +157,7 @@ checkTrue <- function(expr, msg="")
   ##@codestatus : testing
 
   if (missing(expr)) {
-    stop("'expr' is missing.")
+    stop("'expr' is missing")
   }
   if(exists(".testLogger", envir=.GlobalEnv)) {
     .testLogger$incrementCheckNum()
@@ -173,9 +171,8 @@ checkTrue <- function(expr, msg="")
     if(exists(".testLogger", envir=.GlobalEnv)) {
       .testLogger$setFailure()
     }
-    stop("Test not TRUE.\n", msg)
-  }
-  else {
+    stop("Test not TRUE\n", msg)
+  } else {
     return(TRUE)
   }
 }
@@ -191,14 +188,17 @@ checkException <- function(expr, msg="", silent=FALSE)
   ##  adding argument silent was suggested by Seth Falcon <sfalcon@fhcrc.org>
   ##  who provided a patch.
   ##@edescr
-  ##@in  func   : [parameterless function] the function to be checked
+  ##@in  expr   : [parameterless function] the function to be checked
   ##@in  msg    : [character] an optional message to further identify and document the call
   ##@in  silent : [logical] passed on to try, iff TRUE error messages will be suppressed 
   ##
   ##@ret        : [logical] TRUE, if evaluation of the expression results in a 'try-error', else a stop signal is issued 
   ##
   ##@codestatus : testing
-  
+
+  if (missing(expr)) {
+    stop("'expr' is missing")
+  }
   if(exists(".testLogger", envir=.GlobalEnv)) {
     .testLogger$incrementCheckNum()
   }
@@ -207,9 +207,8 @@ checkException <- function(expr, msg="", silent=FALSE)
     if(exists(".testLogger", envir=.GlobalEnv)) {
       .testLogger$setFailure()
     }
-    stop("Error not generated as expected.\n", msg)
-  }
-  else {
+    stop("Error not generated as expected\n", msg)
+  } else {
     return(TRUE)
   }
 }
@@ -220,14 +219,13 @@ DEACTIVATED <- function(msg="")
   ##@bdescr
   ##  Convenience function, for maintaining test suites.
   ##  If placed in an existing test case call
-  ##  the test will be executed normally until (so all code will be checked and
-  ##  errors or failures reported as usual) occurance of the call
-  ##  after which execution will leave the test case.
+  ##  the test will be executed normally until occurance of the call
+  ##  after which execution will leave the test case (so all code will
+  ##  be checked and errors or failures reported as usual).
   ##  An entry for a seperate table in the log will be added
   ##  for this test case.
   ##
   ##@edescr
-  ##@in a   : [expression] the logical expression to be checked to be TRUE
   ##@in msg : [character] optional message to further identify and document the call
   ##
   ##@codestatus : testing
