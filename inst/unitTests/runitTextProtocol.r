6e114e5501457a0ed8579596c2a5911af20c7a78
burgerm <>
2012-11-22 22:36:39 +0000

    refactored; added test for missing value precondition checks

--- inst/unitTests/runitTextProtocol.r
+++ inst/unitTests/runitTextProtocol.r
@@ -1,5 +1,5 @@
 ##  RUnit : A unit test framework for the R programming language
-##  Copyright (C) 2003-2009  Thomas Koenig, Matthias Burger, Klaus Juenemann
+##  Copyright (C) 2003-2012  Thomas Koenig, Matthias Burger, Klaus Juenemann
 ##
 ##  This program is free software; you can redistribute it and/or modify
 ##  it under the terms of the GNU General Public License as published by
@@ -19,41 +19,63 @@
 
 cat("\n\nRUnit test cases for 'textProtocol' function\n\n")
 
+.setUp <- function() {
+	##  copy baseenv() logger
+	tmp <- get(".testLogger", envir = .GlobalEnv)
+	testCaseDir <- file.path(system.file(package="RUnit"), "examples")
+	testSuiteInternal <- defineTestSuite("RUnit Self Test", testCaseDir, "correctTestCase.r")
+	testData2 <<- runTestSuite(testSuiteInternal, useOwnErrorHandler=FALSE)
+	
+	timeStamp <- format(Sys.time(), "%y%m%d-%H%M")
+	testProtocolFile <<- file.path(tempdir(), paste(timeStamp, "test_printHTMLProtocol.txt", sep="_"))
+	
+	assign(".testLogger", tmp, envir = .GlobalEnv)
+	
+}
+
+
+.tearDown <- function() {
+	
+	rm(list=c("testData2", "testProtocolFile"), envir = .GlobalEnv)
+}
+
 
 testRUnit.printTextProtocol <- function()
 {
-  ##  copy baseenv() logger
-  tmp <- get(".testLogger", envir = .GlobalEnv)
-  testCaseDir <- file.path(system.file(package="RUnit"), "examples")
-  testSuiteInternal <- defineTestSuite("RUnit Self Test", testCaseDir, "correctTestCase.r")
-  testData2 <- runTestSuite(testSuiteInternal, useOwnErrorHandler=FALSE)
-
-  timeStamp <- format(Sys.time(), "%y%m%d-%H%M")
-  testProtocolFile <- file.path(tempdir(), paste(timeStamp, "test_printHTMLProtocol.txt", sep="_"))
+	
   ret <- printTextProtocol(testData2, fileName=testProtocolFile)
-
-  assign(".testLogger", tmp, envir = .GlobalEnv)
-  
   checkTrue( file.exists(testProtocolFile))
-
-
+  
+  
   ##  input argument error handling
   ##  missing 'testData' object
   checkException(printTextProtocol())
 
   ##  wrong class
   checkException(printTextProtocol("dummy"))
-
   
-  ##  fileName arg errors
+  myTestData <- vector(mode="list", 3)
+  for (i in seq_along(myTestData)) {
+	  myTestData[[i]]$nErr <- i
+	  myTestData[[i]]$nDeactivated <- i - 1
+	  myTestData[[i]]$nTestFunc <- i*13
+	  myTestData[[i]]$nFail <- i + 3
+  }
+  class(myTestData) <- "myTestData"
+  checkException(printTextProtocol(myTestData))
+  
+  
   testData <- list()
   class(testData) <- "RUnitTestData"
+
+  ##  fileName arg errors
   ##  wrong type
   checkException(printTextProtocol(testData, fileName=numeric(1)))
   ##  wrong length
   checkException(printTextProtocol(testData, fileName=character(0)))
   checkException(printTextProtocol(testData, fileName=character(2)))
-
+  ##  NA
+  checkException(printTextProtocol(testData, fileName=as.character(NA)))
   
   ##  separateFailureList arg errors
   ##  wrong type
@@ -61,6 +83,8 @@ testRUnit.printTextProtocol <- function()
   ##  wrong length
   checkException(printTextProtocol(testData, separateFailureList=logical(0)))
   checkException(printTextProtocol(testData, separateFailureList=logical(2)))
+  ##  NA
+  checkException(printTextProtocol(testData, separateFailureList=as.logical(NA)))
 
   ##  showDetails arg errors
   ##  wrong type
@@ -68,5 +92,17 @@ testRUnit.printTextProtocol <- function()
   ##  wrong length
   checkException(printTextProtocol(testData, showDetails=logical(0)))
   checkException(printTextProtocol(testData, showDetails=logical(2)))
+  ##  NA
+  checkException(printTextProtocol(testData, showDetails=as.logical(NA)))
 }
 
+
+testRUnit.print <- function()
+{
+	checkTrue( is.null(print(testData2)))
+}
+
+testRUnit.summary <- function()
+{
+	checkTrue( summary(testData2))
+}
